fb705770c584235023b533054c2b89da987aa9ae
Evgeny <evgr926@outlook.com>
2024-06-24 20:27:09 +0300

    111

---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private int i = 0;
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                Color[] color = new Color[7];
+                color[0] = Color.FromRgb(255, 0, 0);
+                color[1] = Color.FromRgb(0, 255, 0);
+                color[2] = Color.FromRgb(0, 0, 255);
+                color[3] = Color.FromRgb(0, 0, 0);
+                color[4] = Color.FromRgb(255, 255, 255);
+                color[5] = Color.FromRgb(255, 251, 0);
+                color[6] = Color.FromRgb(252, 0, 255);
+                frme1.Background = new SolidColorBrush(color[i]);
+                i++;
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                MessageBox.Show("Тест монитора завершен");
+                this.Close();
+            }
+
+        }
+
+        private void Dt_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                Color[] color = new Color[7];
+                color[0] = Color.FromRgb(255, 0, 0);
+                color[1] = Color.FromRgb(0, 255, 0);
+                color[2] = Color.FromRgb(0, 0, 255);
+                color[3] = Color.FromRgb(0, 0, 0);
+                color[4] = Color.FromRgb(255, 255, 255);
+                color[5] = Color.FromRgb(255, 251, 0);
+                color[6] = Color.FromRgb(252, 0, 255);
+                frme1.Background = new SolidColorBrush(color[i]);
+                i++;
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                MessageBox.Show("Тест монитора завершен");
+                this.Close();
+            }
+        }
+    }
+}
---
+++ WpfApp1/Window3.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+
+        }
+        
+        private void timerTick(object sender, EventArgs e)
+        {
+            driveinf.Content = null;
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo d in allDrives)
+            {
+                driveinf.Content += Convert.ToString($"Диск {d}, {d.Name}  {'\n'}" +
+                    $" Тип диска: {d}, {d.DriveType} {'\n'}");
+                    
+                if (d.IsReady)
+                {
+                    driveinf.Content += Convert.ToString($"Доступно на диске: {d.TotalFreeSpace} байт  {'\n'}" +
+                        $"Размер диска: {d.TotalSize} байт");
+                }
+                driveinf.Content += $"{'\n'} {'\n'}";
+            }
+            
+        }
+    }
+}
